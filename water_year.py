d5f1c18a9bbd02c05190e9be666deae67f687f6c
Paul <paulinkenbrandt@utah.gov>
2019-12-05 12:55:46 -0700

    Create water_year.py
    water year calculator

---
+++ water_year.py
@@ -0,0 +1,7 @@
+def calc_wat_year(df):
+  df['water year'] = df.index.shift(-9,freq='M').year+1
+  df['ones'] = 1
+  df['water year doy'] = df['ones'].groupby(df['water year']).cumsum()
+  df['doylen'] = df['ones'].groupby(df['water year']).count()
+  df['water year doy1'] = df.apply(lambda df: df['doy']-273 if df['water year'] > df['year'] else df['doy']+92,1)
+  return df
